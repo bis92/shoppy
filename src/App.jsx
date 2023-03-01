ca73d4abce22a1d09b85696cf35ce73624d51b9c
Bis92 <bis920309@gmail.com>
2023-03-01 15:15:15 +0900

    shoppy project init

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { Outlet } from 'react-router-dom';
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h2>Header</h2>
+      </header>
+      <Outlet
+
+      />
+    </div>
+  );
+}
+
--- src/index.js
+++ src/index.js
@@ -3,12 +3,35 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import NotFound from './page/NotFound';
+import Products from './page/Products';
+import ProductDetail from './page/ProductDetail';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <NotFound />,
+    children: [
+      {
+        path: "products",
+        element: <Products />
+      },
+      {
+        path: "products/:productId",
+        element: <ProductDetail />
+      }
+    ]
+  }
+])
+
 root.render(
-  <React.StrictMode>
+  <RouterProvider router={router}>
     <App />
-  </React.StrictMode>
+  </RouterProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
